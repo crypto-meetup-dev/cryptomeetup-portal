d0d0b8266f8ca5c546f25876ba9c98aac9f988eb
gloria12345678 <quyi28266687@gmail.com>
2018-10-25 16:03:57 +0800

    jp

---
+++ src/i18n/jp.js
@@ -0,0 +1,11 @@
+export default {
+    switch_lang: '他の言語',
+    English: '英語',
+    Chinese: '中国語 ',
+    world_view: '世界地図',
+    filter_country_or_region: '国家とエリアの選択',
+    login: 'ログイン',
+    logout: 'ログアウト ',
+    back:'戻り',
+  };
+  
