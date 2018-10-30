8297323f21122c84848f514a6ba4a40d9860b14b
Frank Wei <frank@frankwei.xyz>
2018-10-30 16:11:25 +0800

    linter fixed

--- src/store/index.js
+++ src/store/index.js
@@ -88,9 +88,9 @@ export default new Vuex.Store({
     async updateMarketInfoAsync({ commit }) {
       try {
         const marketInfoTable = await API.getMarketInfoAsync();
-        var marketInfo = marketInfoTable[0];
-        marketInfo.coin_price = ((parseFloat(marketInfo.supply.split(' ')[0]))/10000000000).toFixed(4).toString() + " EOS";
-        marketInfo.supply = (parseFloat(marketInfo.supply.split(' ')[0]) - 40000000).toFixed(4).toString() + " CMU";
+        const marketInfo = marketInfoTable[0];
+        marketInfo.coin_price = `${((parseFloat(marketInfo.supply.split(' ')[0])) / 10000000000).toFixed(4).toString()} EOS`;
+        marketInfo.supply = `${(parseFloat(marketInfo.supply.split(' ')[0]) - 40000000).toFixed(4).toString()} CMU`;
         // price, balance, coin_price
         commit('setMarketInfo', marketInfo);
       } catch (err) {
