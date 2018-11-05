dcaccbc74e0c8bbb24faedb7703ea4e0402b8f97
Adappdeveloper <1dappdeveloper@gmail.com>
2018-11-05 14:35:52 +0800

    Update src/App.vue
    addtional explanation of the prize pool

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,7 @@
               </div>
               <div style="display:flex;align-items:center;">
                 <button style="margin-right:10px" class="button" @click="claim">{{$t('claim_btn')}}</button>
-                <b-tooltip label="游戏说明游戏说明游戏说明游戏说明游戏说明游戏说明游戏说明游戏说明游戏说明游戏说明游戏说明游戏说明"
+                <b-tooltip label=""
                     position="is-right" :multilined="true" size="is-large">
                     <b-icon class="question-icon" pack="fas" type="is-white" icon="question-circle" size="is-middle"></b-icon>
                 </b-tooltip>
@@ -105,8 +105,8 @@
       <div class="footer-item" v-if="globalInfo">{{$t('last_buyer')}}: <b>{{ globalInfo.last | moment('calendar') }}</b> </div>
       <div class="footer-item" v-if="globalInfo">{{$t('count_down')}}: <b>{{ globalCountdown }}</b> </div>
       <div class="footer-item" v-if="globalInfo">{{$t('prize_pool')}}: <b>{{ (globalInfo.pool / 10000).toFixed(4).toString() }} CMU </b> </div>
-      <b-tooltip label="这些CMU值?的EOS，可以在Kyubey.network来进行人人或人机兑换大量EOS"
-                    position="is-right" :multilined="true" size="is-large">
+      <b-tooltip label="Exchange CMU to EOS via https://kyubey.network/Token/CMU/exchange "
+                    position="is-left" :multilined="true" size="is-large">
                     <b-icon class="question-icon" pack="fas" type="is-white" icon="question-circle" size="is-middle"></b-icon>
       </b-tooltip>
       <div class="footer-item is-hidden-mobile">
