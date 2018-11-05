6eb0c6e43485cad0fc57b92944487007fe4cdef2
lychees <lychees67@gmail.com>
2018-11-05 12:12:26 +0800

    Cont.

--- src/components/SponsorPaymentModal.vue
+++ src/components/SponsorPaymentModal.vue
@@ -83,7 +83,7 @@ export default {
     walletTransferData() {
       const payload = {
         to: this.transaction.to,
-        amount: this.transaction.amount / 10000,
+        amount: (this.transaction.amount / 10000).toFixed(4),
         contract: 'eosio.token',
         symbol: 'EOS',
         precision: 4,
