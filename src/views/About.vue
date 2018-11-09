a458e169e072c90724848315a01c533cad297676
Deasoso <932152571@qq.com>
2018-11-09 14:30:37 +0800

    Standalone Token\About view.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div :class="['country-detail', {'is-active': aboutShow}]">
+    <div class="globe-control">
+      <div style="position: absolute;top: 2rem;left: 5rem;">
+        <button class="globe-control-item button is-hidden-mobile is-white is-small is-rounded is-outlined"
+                v-show="aboutShow"
+                @click="aboutShow=!aboutShow"
+        >
+          <b-icon icon="arrow-left" size="is-small" />&nbsp;{{$t('back')}}
+        </button>
+      </div>
+
+    </div>
+    <h1  v-show="aboutShow">
+      <div class="content"
+        v-html="$t('ABOUT_CONTENT')">
+      </div>
+    </h1>
   </div>
 </template>
