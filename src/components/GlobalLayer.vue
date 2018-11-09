b5f6141deae2f3fc74426d38ef260b4bb8dca0ae
crazycoder <crazycoder1988@outlook.com>
2018-11-09 22:44:37 +0800

    Fix new page loading

---
+++ src/components/GlobalLayer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="layer">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GlobalLayer',
+};
+</script>
+<style lang="sass" scoped>
+.layer
+  position: fixed
+  left: 0
+  top: 0
+  width: 100%
+  height: 100%
+  display: flex
+  align-items: center
+  justify-content: center
+  background: rgba(#000, 0.6)
+  z-index: 999
+</style>
