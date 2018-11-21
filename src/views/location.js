42a899b5087d7d58d8516be696de06be51d0a41c
webkonglong <1250888888@qq.com>
2018-11-21 17:36:50 +0800

    添加聚合 (#144)
    * [update] 创建地标从新封装 流程跑通
    
    * 更改为聚合

--- src/views/location.js
+++ src/views/location.js
@@ -11,14 +11,13 @@ import { ajax, analysis } from '@/util/ajax'
 import MyLocationComp from '@/components/landmark/MapMarkerLocation.vue'
 import createLocation from '@/components/landmark/createLocation.vue'
 import LocationPopupComp from '@/components/landmark/LocationPopup.vue'
-import MapMarkerMeetup from '@/components/landmark/MapMarkerMeetup.vue'
 
 const location = {
   map: null,
   myLocation: null,
   myLocationMarker: null,
   createLocationPopup: null,
-  locationArr: [],
+  locationArr: null,
   locationPopup: null,
   locationPopupFn: null,
   locationPopupComp: null,
@@ -28,17 +27,17 @@ const location = {
     this.getMyLocation()
   },
   getMyLocation () {
-    // navigator.geolocation.getCurrentPosition((position) => {
-    //   const coord = [position.coords.longitude, position.coords.latitude]
-    //   console.log(coord, 'coord')
-    //   this.addMyLocationComp(coord)
-    //   this.getLocationArr(coord)
-    // })
+    navigator.geolocation.getCurrentPosition((position) => {
+      const coord = [position.coords.longitude, position.coords.latitude]
+      console.log(coord)
+      this.addMyLocationComp(coord)
+      this.getLocationArr(coord)
+    })
     // 获取用户坐标
-    const coord = [116.478515, 39.889992]
-    this.myLocationNum = coord
-    this.addMyLocationComp(coord)
-    this.getLocationArr(coord)
+    // const coord = [116.478515, 39.889992]
+    // this.myLocationNum = coord
+    // this.addMyLocationComp(coord)
+    // this.getLocationArr(coord)
   },
   addMyLocationComp (coord) {
     // 添加我的位置的icon
@@ -59,26 +58,8 @@ const location = {
       closeOnClick: false,
       className: 'create-location-popup'
     }).setDOMContent(createLocationComp.$mount().$el).setLngLat(coord).addTo(this.map)
-  },
-  openCreatePopup(coord) {
-    // 打开创建地标的ui
-    this.createLocationPopup.remove()
-    this.locationPopupComp.$mount().setData(null)
-    this.locationPopupFn.setLngLat(coord).addTo(this.map)
-  },
-  getLocationArr () {
-    // 获取所有地标数组
-    ajax.get(analysis('/pub/bt/point/distance', {
-      latitude: '30.275029',
-      longitude: '119.990402',
-      distance: 2000,
-    })).then(resp => {
-      this.locationArr = resp && resp.records
-      this.initLocationPopup()
-    })
-  },
-  initLocationPopup () {
-    // 初始化所有地标ui
+
+    // 添加展示地标以及创建地标popup
     this.locationPopupComp = new Vue(LocationPopupComp)
     this.locationPopupComp.$mount().$on('createLocation', () => {
       this.createLocationSuccess()
@@ -87,28 +68,115 @@ const location = {
       offset: 25,
       closeButton: false,
     }).setDOMContent(this.locationPopupComp.$mount().$el);
+  },
+  openCreatePopup(coord) {
+    // 打开创建地标的ui
+    this.createLocationPopup.remove()
+    this.locationPopupComp.$mount().setData(null)
+    this.locationPopupFn.setLngLat(coord).addTo(this.map)
+  },
+  formatData (data) {
+    const features = []
+    data.length && data.forEach(item => {
+      features.push({
+        type: 'Feature',
+        properties: item,
+        geometry: {
+          type: 'Point',
+          coordinates: [item.longitude, item.latitude]
+        }
+      })
+    })
+    this.locationArr = {
+      type: 'FeatureCollection',
+      features,
+    }
+
     this.renderLocation()
   },
-  renderLocation () {
-    // 渲染所有地标
-    const locationDOM = {}
-    this.locationArr.length && this.locationArr.forEach((locationItem, i) => {
-      locationDOM[`index${i}`] = new Vue({
-        ...MapMarkerMeetup,
-        propsData: {
-          coord: locationItem,
-        },
-      }).$mount().$on('click', data => {
-        this.locationPopupComp.setData(data.infos.length ? data.infos[0] : null);
-        this.locationPopupFn.setLngLat([+data.longitude, +data.latitude]);
-        this.map.flyTo({ center: [+data.longitude, +data.latitude], zoom: 15 });
-      })
-      new mapboxgl.Marker(locationDOM[`index${i}`].$el).setLngLat([+locationItem.longitude, +locationItem.latitude]).setPopup(this.locationPopupFn).addTo(this.map);
+  getLocationArr () {
+    // 获取所有地标数组
+    ajax.get(analysis('/pub/bt/point/distance', {
+      latitude: '30.275029',
+      longitude: '119.990402',
+      distance: 2000,
+    })).then(resp => {
+      resp.records && this.formatData(resp.records)
     })
+  },
+  renderLocation () {
+    this.map.addSource('earthquakes', {
+      type: 'geojson',
+      data: this.locationArr,
+      cluster: true,
+      clusterMaxZoom: 14,
+      clusterRadius: 50
+    })
+
+    this.map.addLayer({
+      id: "clusters",
+      type: "circle",
+      source: "earthquakes",
+      filter: ["has", "point_count"],
+      paint: {
+        "circle-color": ["step", ["get", "point_count"], "#8d7da3", 100, "#3a3146", 750, "#213b13"],
+        "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40]
+      }
+    })
+
+    this.map.addLayer({
+      id: "cluster-count",
+      type: "symbol",
+      source: "earthquakes",
+      filter: ["has", "point_count"],
+      layout: {
+        "text-field": "{point_count_abbreviated}",
+        "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+        "text-size": 18,
+      }
+    })
+
+    this.map.addLayer({
+      id: "unclustered-point",
+      type: "circle",
+      source: "earthquakes",
+      filter: ["!", ["has", "point_count"]],
+      paint: {
+        "circle-color": "#abcdef", // transparent
+        "circle-radius": 8,
+        "circle-stroke-width": 3,
+        "circle-stroke-color": "#abcdef"
+      }
+    })
+
+    this.map.on('click', 'clusters', e => {
+      const features = this.map.queryRenderedFeatures(e.point, { layers: ['clusters'] })
+      const clusterId = features[0].properties.cluster_id
+      this.map.getSource('earthquakes').getClusterExpansionZoom(clusterId, (err, zoom) => {
+        if (err) {
+          return
+        }
+        this.map.easeTo({
+          center: features[0].geometry.coordinates,
+          zoom: zoom
+        })
+      })
+    })
+
+    this.map.on('click', 'unclustered-point', e => {
+      const features = this.map.queryRenderedFeatures(e.point, { layers: ['unclustered-point'] })
+      const data = JSON.parse(features[0].properties.infos)
+      this.openLocationPopup(features[0].properties)
+    })
+  },
+  openLocationPopup(features) {
+    const data = JSON.parse(features.infos)
+    this.locationPopupComp.$mount().setData(data[0])
+    this.locationPopupFn.setLngLat([features.longitude, features.latitude]);
+    this.locationPopupFn.addTo(this.map)
   },
   createLocationSuccess () {
     // 创建地标成功
-    console.log('xxxx', this.locationPopupFn, this.locationPopupFn.remove)
     this.locationPopupFn.remove()
     this.createLocationPopup.setLngLat(this.myLocationNum).addTo(this.map)
   }
