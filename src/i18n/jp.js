4cd1965367b3386b034977749182d3049a8fd4d8
管超 <1242256806@qq.com>
2018-11-21 18:20:02 +0800

    fix lang

--- src/i18n/jp.js
+++ src/i18n/jp.js
@@ -3,11 +3,11 @@ import ABOUT_CONTENT from './about_en.html';
 
 export default {
   switch_lang: '他の言語',
-  English: '英語',
-  Chinese: '中国語',
+  English: 'English',
+  Chinese: '中文',
   Japanese: '日本語',
-  Korean: '韩语',
-  Russian:'ロシア語',
+  Korean: '한국어',
+  Russian:'русский',
   Globe: '地球',
   Map: '地図',
   token_view: 'トークン',
