609d9b8055678aa50adaddde3cc128dbcee2004a
amz <1250888888@qq.com>
2018-11-23 22:33:35 +0800

    [update] 地图定位更换高德

--- src/views/location.js
+++ src/views/location.js
@@ -5,6 +5,8 @@
 //   }
 
 // }
+
+
 import mapboxgl from 'mapbox-gl'
 import Vue from 'vue'
 import { ajax, analysis } from '@/util/ajax'
@@ -34,19 +36,34 @@ const location = {
     this.interval()
   },
   getMyLocation () {
-    const geolocation = new BMap.Geolocation()
     const slef = this
-    geolocation.getCurrentPosition(function (r) {
-      if (this.getStatus() === BMAP_STATUS_SUCCESS) {
-        const coord = [r.point.lng, r.point.lat]
-        slef.map.flyTo({ center: coord, zoom: 13 })
-        slef.myLocationNum = coord
-        slef.addMyLocationComp(coord)
-        slef.getLocationArr(coord)
-      } else {
-        slef.errorCallback(`failed: ${this.getStatus()}`)
-      }
-    }, { enableHighAccuracy: true })
+    const map = new AMap.Map('container', {
+      resizeEnable: true
+    });
+
+    AMap.plugin('AMap.Geolocation', function () {
+      var geolocation = new AMap.Geolocation({
+        enableHighAccuracy: true,//是否使用高精度定位，默认:true
+        timeout: 10000,          //超过10秒后停止定位，默认：5s
+        buttonPosition: 'RB',    //定位按钮的停靠位置
+        buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+        zoomToAccuracy: true,   //定位成功后是否自动调整地图视野到定位点
+
+      });
+
+      map.addControl(geolocation);
+      geolocation.getCurrentPosition(function (status, result) {
+        if (status == 'complete') {
+          const coord = [result.position.lng, result.position.lat]
+          slef.map.flyTo({ center: coord, zoom: 13 })
+          slef.myLocationNum = coord
+          slef.addMyLocationComp(coord)
+          slef.getLocationArr(coord)
+        } else {
+          slef.errorCallback(result.message)
+        }
+      });
+    });
   },
   addMyLocationComp (coord) {
     // 添加我的位置的icon
