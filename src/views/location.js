431b083ff88bc5f8b3ae5916823ec3d6808b55d6
webkonglong <1250888888@qq.com>
2018-11-24 18:12:02 +0800

    更新地理位置 (#166)
    * [update] 创建地标从新封装 流程跑通
    
    * 更改为聚合
    
    * [update] 调试通了登录
    
    * [update]
    
    * update api
    
    * [update] 更新地标API修改
    
    * [update] 地标添加动画 登录移动到地图页面
    
    * [update] 修改地标ui
    
    * [update] 未登录不显示创建地标按钮
    
    * [update] 暂时不获取地理位置，直接写死了一个地理位置
    
    * 增加获取不到用户坐标 报错提示
    
    * [update] 定位更换为百度
    
    * [update] 地图定位更换高德
    
    * [bug]之前获取不到位置 写死了位置，忘记了api 坑了自己
    
    * [update] 新增点击图片放大， 用户看不到别人 审核中的地标
    
    * cl
    
    * 更新坐标

--- src/views/location.js
+++ src/views/location.js
@@ -272,6 +272,38 @@ const location = {
   getData () {
     this.isGetData = true
     this.isGetMylocation && this.getLocationArr(this.myLocationNum)
+  },
+  updateLocation () {
+    if (this.map) {
+      const slef = this
+      const map = new AMap.Map('container', {
+        resizeEnable: true
+      });
+
+      AMap.plugin('AMap.Geolocation', function () {
+        var geolocation = new AMap.Geolocation({
+          enableHighAccuracy: true,//是否使用高精度定位，默认:true
+          timeout: 10000,          //超过10秒后停止定位，默认：5s
+          buttonPosition: 'RB',    //定位按钮的停靠位置
+          buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+          zoomToAccuracy: true,   //定位成功后是否自动调整地图视野到定位点
+
+        });
+
+        map.addControl(geolocation);
+        geolocation.getCurrentPosition(function (status, result) {
+          if (status == 'complete') {
+            const coord = [result.position.lng, result.position.lat]
+            slef.map.flyTo({ center: coord, zoom: 13 })
+            slef.myLocationNum = coord
+            slef.myLocationMarker.setLngLat(coord).addTo(slef.map)
+            slef.createLocationPopup.setLngLat(coord).addTo(slef.map)
+          } else {
+            slef.errorCallback(result.message)
+          }
+        });
+      });
+    }
   }
 }
 
