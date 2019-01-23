fda5020674ebb3b8e84b5208ef3dcc01a4c638dd
amz <1250888888@qq.com>
2019-01-23 14:09:14 +0800

    modules config

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import Land from '@/util/land';
 import getApi from '@/util/apis/index.js'
 import ui from './ui';
 import Global from '@/Global.js';
+import modules from '@/config/modules.js';
 
 Vue.use(Vuex);
 
@@ -13,6 +14,7 @@ export default new Vuex.Store({
     ui,
   },
   state: {
+    modulesConfig: modules,
     contractType: 'bos',
     isScatterConnected: false,
     scatterAccount: null,
