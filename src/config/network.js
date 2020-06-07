289b498f13effddd3e2f2b69d4ba767a34717b2b
lychees <lychees67@gmail.com>
2020-06-06 20:45:10 -0700

    Update network.js

--- src/config/network.js
+++ src/config/network.js
@@ -2,7 +2,7 @@ export const networks = {
   eos: {
     protocol: 'https',
     blockchain: 'eos',
-    host: 'api.eosbeijing.one',
+    host: 'api.eosnewyork.io',
     port: 443,
     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
   },
