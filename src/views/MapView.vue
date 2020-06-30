bab204e5ed8955b761f1b09eedce891c2bd04f65
Ayaka Neko <neko@ayaka.moe>
2020-06-30 20:09:30 +0800

    Made the hover effect for all of each country
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -37,6 +37,9 @@ import location from './location.js'
 import { mapState } from 'vuex'
 import { setLocalStorage, removeLocalStorage } from '@/util/storeUtil.js'
 
+// const geoData = require('./customgeo.json')
+const geoData = require('./customgeo.json')
+
 export default {
   name: 'map-view',
   data() {
@@ -171,8 +174,85 @@ export default {
       //     },
       //   });
       // });
- 
+
       try {
+        let hoveredStateId = null;
+
+        map.addSource('countries', {
+          type: 'geojson',
+          data: geoData
+        });
+ 
+        // The feature-state dependent fill-opacity expression will render the hover effect
+        // when a feature's hover state is set to true.
+        map.addLayer({
+          id: 'state-fills',
+          type: 'fill',
+          source: 'countries',
+          layout: {},
+          paint: {
+            'fill-color': '#000',
+            'fill-opacity': [
+              'case',
+              ['boolean', ['feature-state', 'hover'], false],
+              0.8,
+              0
+            ]
+          }
+        });
+ 
+        map.addLayer({
+          id: 'state-borders',
+          type: 'line',
+          source: 'countries',
+          layout: {},
+          paint: {
+            'line-color': '#FFD83F',
+            'line-width': 2,
+            'line-opacity': [
+              'case',
+              ['boolean', ['feature-state', 'hover'], false],
+              0.5,
+              0
+            ]
+
+          }
+        });
+ 
+        // When the user moves their mouse over the state-fill layer, we'll update the
+        // feature state for the feature under the mouse.
+        map.on('mousemove', 'state-fills', (e, i) => {
+          console.log('moved!')
+          if (e.features.length > 0) {
+            if (hoveredStateId) {
+              map.setFeatureState(
+                { source: 'countries', id: hoveredStateId },
+                { hover: false }
+              );
+            }
+            hoveredStateId = e.features[0].id;
+            map.setFeatureState(
+              { source: 'countries', id: hoveredStateId },
+              { hover: true }
+            );
+          }
+          console.log('length > 0: ', e.features[0].layer.paint['fill-opacity'][1][2])
+          console.log('length > 0: ', e.features[0].layer)
+        });
+ 
+        // When the mouse leaves the state-fill layer, update the feature state of the
+        // previously hovered feature.
+        map.on('mouseleave', 'state-fills', () => {
+          console.log('leaved!')
+          if (hoveredStateId) {
+            map.setFeatureState(
+              { source: 'countries', id: hoveredStateId },
+              { hover: false }
+            );
+          }
+          hoveredStateId = null;
+        });
+
         map.loadImage(
           'https://neko.ayaka.moe/image/avatar.jpeg',
           (error, image) => {
@@ -199,7 +279,7 @@ export default {
               source: 'point',
               layout: {
                 'icon-image': 'cat',
-                'icon-size': 0.25
+                'icon-size': 0.06
               }
             });
           }
@@ -208,10 +288,10 @@ export default {
         console.log(e)
       }
 
-      if ('geolocation' in navigator) {
-        this.locationUpdateTimer = setInterval(() => this.updateLocation(), 5000);
-        this.updateLocation();
-      }
+      // if ('geolocation' in navigator) {
+      //   this.locationUpdateTimer = setInterval(() => this.updateLocation(), 5000);
+      //   this.updateLocation();
+      // }
     },
 
     updateCheckInAvailability(lonLat) {
