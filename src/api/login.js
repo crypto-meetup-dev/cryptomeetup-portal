ba52cafa8ee58abd9738cec35d0e6b0aa207bc1e
Ayaka Neko <neko@ayaka.moe>
2020-07-03 16:20:12 +0800

    Seperated for dev and production files
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/login.js
+++ src/api/login.js
@@ -1,6 +1,13 @@
 import client from './index';
+import { property } from 'lodash';
 
-const pathConfig = require('../config/env.dev.json')
+const fs = require('fs')
+const path = require('path')
+const config = require('../../config.json')
+
+const devPath = path.resolve('../config/env.dev.json')
+const proPath = path.resolve('../config/env.json')
+const pathConfig = config.development ? JSON.parse(fs.readFile(devPath)) : JSON.parse(fs.readFile(proPath))
 
 export function loginWithEmail(username, password) {
   return client.post(pathConfig.MATATAKIAPI + '/login/account', { username, password });
