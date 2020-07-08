255c4a197c44f1c619118ded38cea08205743d42
Ayaka Neko <neko@ayaka.moe>
2020-07-08 18:37:22 +0800

    Use dotenv now
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/login.js
+++ src/api/login.js
@@ -1,29 +1,14 @@
 import client from './index';
-import { property } from 'lodash';
-
-const fs = require('fs')
-const path = require('path')
-const config = require('../../config.json')
-
-const devPath = require('../config/env.dev.json')
-const proPath = require('../config/env.json')
-
-let pathConfig = ''
-if (config.development) {
-  pathConfig = devPath
-} else {
-  pathConfig = proPath
-}
 
 export function loginWithEmail(username, password) {
-  return client.post(pathConfig.MATATAKIAPI + '/login/account', { username, password });
+  return client.post(process.env.VUE_APP_MATATAKIAPI + '/login/account', { username, password });
 }
 
 export function getUserProfile(uid) {
-  return client.get(pathConfig.MATATAKIAPI + `/user/${uid}`);
+  return client.get(process.env.VUE_APP_MATATAKIAPI + `/user/${uid}`);
 }
 
 export function getAvatarUrl(location) {
-  const imageCDNServer = pathConfig.MTTKIMGCDN;
+  const imageCDNServer = process.env.VUE_APP_MTTKIMGCDN;
   return `${imageCDNServer}${location}`;
 }
